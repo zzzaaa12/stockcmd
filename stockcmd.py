b693550e4bae962f063df1e441885ed054bc8343
Alex_Lin <Alex_Lin@cameo.com.tw>
2016-05-03 23:23:27 +0800

    add a parameter to list all information

--- stockcmd.py
+++ stockcmd.py
@@ -21,11 +21,15 @@ def usage():
     print '    stockcmd.py [Options] [stock numbers]'
     print ''
     print 'Options:'
+    print '    -a: list all stock infomation (include TSE, OTC, other stock)'
     print '    -i: list include TSE index and OTC index'
     print '    -w: list International Stock Indexes'
     print ''
     print 'Example:'
-    print '    stockcmd.py -a 2330 2317 3008'
+    print '    stockcmd.py -i'
+    print '    stockcmd.py -w'
+    print '    stockcmd.py -i 2330 2317 3008'
+    print '    stockcmd.py 2330 2317 3008'
     print ''
 
 
@@ -75,70 +79,80 @@ def world_index():
             ratio = '+' + ratio
 
         print list[i][1] + '\t\t' + '{0:8s}'.format(j["l"]) + '\t' + j["c"] + '\t\t' + ratio + '%'
+    print ''
 
 
 if len(argv) == 1:
+    # no parameter, show usage()
     usage()
     exit()
 
 elif len(argv) > 1:
     del argv[0]
     i = 0
+    add_world = False
+    add_twse = False
 
+    # read parameters
     for x in argv:
-
-        if str(x) == '-w':
-            world_index()
-            exit()
-
-        if str(x) == '-h' or str(x) == '--help':
+        if str(x) == '-a':
+            add_world = True;
+            add_twse = True;
+        elif str(x) == '-w':
+            add_world = True;
+        elif str(x) == '-i':
+            add_twse = True;
+        elif str(x) == '-h' or str(x) == '--help':
             usage()
             exit()
 
-        if str(x) == '-i':
-            url = url + 'tse_t00.tw|otc_o00.tw|'
-            count += 2
-            del argv[i]
-        i += 1
+    if add_world:
+        world_index()
 
+    if add_twse:
+        url = url + 'tse_t00.tw|otc_o00.tw|'
+        count += 2
+
+    # determine stock is in tse or otc and add it to list
     for x in argv:
         if search_stock(str(x), otc_file):
             url = url + 'otc_' + str(x) + '.tw|'
         elif search_stock(str(x), tse_file):
             url = url + 'tse_' + str(x) + '.tw|'
         else:
-            print str(x) + ': wrong parameter'
             continue
         count += 1
 
-# access the index first and then send request for stock list
-r = requests.session()
-r.get('http://' + server + '/stock/index.jsp', headers = {'Accept-Language':'zh-TW'}, timeout=2)
-result = r.get(url)
+if count > 0:
+    # access the index first and then send request for stock list
+    r = requests.session()
+    r.get('http://' + server + '/stock/index.jsp', headers = {'Accept-Language':'zh-TW'}, timeout=2)
+    result = r.get(url)
 
-# read json data
-json_data = json.loads(result.content)
-print u'股號\t股名\t成交價\t漲跌\t百分比\t成交量\t資料時間'
+    # read json data
+    json_data = json.loads(result.content)
+    print u'股號\t股名\t成交價\t漲跌\t百分比\t成交量\t資料時間'
 
-for i in range(0, count, 1):
-    j = json_data['msgArray'][i]
+    for i in range(0, count, 1):
+        j = json_data['msgArray'][i]
 
-    diff = float(j["z"]) - float(j["y"])
-    if diff > 0:
-        sign = '+'
-    else:
-        sign = ''
+        diff = float(j["z"]) - float(j["y"])
+        if diff > 0:
+            sign = '+'
+        else:
+            sign = ''
 
-    change_str = sign + str(diff)
-    change_str_p = sign + '{0:.2f}'.format(diff / float(j["y"]) *100)
+        change_str = sign + str(diff)
+        change_str_p = sign + '{0:.2f}'.format(diff / float(j["y"]) *100)
 
-    stock_no = j["c"]
-    name = j["n"]
+        stock_no = j["c"]
+        name = j["n"]
 
-    # fix too long name.....
-    if stock_no == 't00':
-        name = u'上市'
-    elif stock_no == 'o00':
-        name = u'上櫃'
+        # fix too long name.....
+        if stock_no == 't00':
+            name = u'上市'
+        elif stock_no == 'o00':
+            name = u'上櫃'
 
-    print j["c"] + '\t' + name + '\t' + j["z"] + '\t' + change_str + '\t' + change_str_p + '%\t' + j["v"] + '\t' + j["t"]
+        print j["c"] + '\t' + name + '\t' + j["z"] + '\t' + change_str + '\t' + change_str_p + '%\t' + j["v"] + '\t' + j["t"]
+    print ''
