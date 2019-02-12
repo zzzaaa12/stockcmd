097fc939bf98227b837d1c070a31723ca8a3df69
Alex.Lin <Alex.Lin@comtrend.com>
2019-02-12 12:05:14 +0800

    update stock/index list

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # taiwan stock setting
-USER_STOCK_LIST = ['2330','2317','3008','6142','00664R','00632R']
+USER_STOCK_LIST = ['2330','2317','3008','6142','00682U','00664R','00666R', '00659R']
 TWSE_SERVER = 'http://220.229.103.179' # mis.twse.com.tw
 TW_FUTURE_URL = 'http://60.250.19.181/Future/FusaQuote_Norl.aspx' # info512.taifex.com.tw
 
@@ -13,10 +13,12 @@ INDEX_LIST = [ \
               ['0000',      'TWSE',      '加權指數'], \
               ['WTXM',      'WTX-All',   '台指期全'], \
               ['USDTWD',    'TWD',       '台幣匯率'], \
-              ['B1YM',      'DOWF',      '小道瓊期'], \
+              ['B1YM',      'DOW-F',     '小道瓊期'], \
+              ['M1NQ',      'NAS-F',     'NASDAQ期'], \
               ['NKI',       'JAPAN',     '日經指數'], \
               ['KOR',       'KOREA',     '韓國指數'], \
               ['A50F',      'A50',       'A50期貨 '], \
+              ['HSI',       'HSI',       '恆生指數'], \
               ['VIX',       'VIX',       'SP500VIX'], \
               ['DAX',       'DAX',       '德國指數'], \
               ['USDINDEX',  'DXY',       '美元指數'], \
@@ -24,6 +26,7 @@ INDEX_LIST = [ \
               ['GOLD',      'Gold',      '黃金現貨'], \
               ['OIL',       'Oil',       '紐約原油'], \
               ['bitcoin',   'BTC',       '比特幣  '], \
+              ['ethereum',  'ETH',       '以太幣  '], \
 #              ['NAS',       'NASDAQ',    '那斯達克'], \
 #              ['SPS',       'S&P500',    'S&P 500 '], \
             ]
