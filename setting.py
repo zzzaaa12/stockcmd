c1e90faac7e7cc73b039cb19a881fda1b3ecc922
Alex.Lin <Alex.Lin@comtrend.com>
2019-03-05 09:21:53 +0800

    Fix WTX future url and update user stock list

--- setting.py
+++ setting.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # taiwan stock setting
-USER_STOCK_LIST = ['2330','2317','3008','6142','00682U','00664R','00666R', '00659R']
+USER_STOCK_LIST = ['2330','2317','3008','6142','00682U','00664R','00666R', '00659R','00671R', '00677U']
 TWSE_SERVER = 'http://220.229.103.179' # mis.twse.com.tw
-TW_FUTURE_URL = 'http://60.250.19.181/Future/FusaQuote_Norl.aspx' # info512.taifex.com.tw
+TW_FUTURE_URL = 'http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx' # info512.taifex.com.tw
 
 
 # world index setting
